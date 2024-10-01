68638beea983a0ff011dee9349771fbd5dae84ab
Ali Aliyev <elvinaliyev724@gmail.com>
2024-10-01 18:27:25 +0400

    ntierarchitecture

---
+++ BlogWebY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogWebY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
