7f48f0d10c2689fe0c6abe6840fdb4f6d31fc594
Ali Aliyev <elvinaliyev724@gmail.com>
2024-10-11 18:31:57 +0400

    Pagination

--- BlogWebY.Entity/obj/Debug/net6.0/BlogWebY.Entity.AssemblyInfo.cs
+++ BlogWebY.Entity/obj/Debug/net6.0/BlogWebY.Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogWebY.Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a20b0575ed732203080989f140c6d5ce2013917")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2500c874181aa67205904f1fcb578729aeb6326")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogWebY.Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogWebY.Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
